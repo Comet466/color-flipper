693585cad9b55cc0c8a04fe7af55aa0236ba8c93
Luis Colina <silkiercomet31@gmail.com>
2020-07-03 11:16:06 -0400

    a little practice with node.js

--- hex.js
+++ hex.js
@@ -1,3 +1,6 @@
+var _ = require('lodash');
+var array = [1,2,3,4,5,6];
+console.log("answer",_.without(array,3));
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
